91291895c5d455846844c8cda87877cce02cae8c
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-10-14 16:40:11 +0300

    footer

--- drama-app/src/components/footer/Footer.jsx
+++ drama-app/src/components/footer/Footer.jsx
@@ -1,7 +1,50 @@
-const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
 
-export default Footer 
+import {
+    FaFacebookF,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin,
+} from "react-icons/fa";
+
+
+import './Footer.css'
+import ContentWrapper from "../ContentWrapper/ContentWrapper";
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <ContentWrapper className="fff">
+               <div className="info">
+               <ul className="menuItems-footer">
+                    <li className="menuItem">Terms Of Use</li>
+                    <li className="menuItem">Privacy-Policy</li>
+                    <li className="menuItem">About</li>
+                    <li className="menuItem">Blog</li>
+                    <li className="menuItem">FAQ</li>
+                </ul>
+               </div>
+                <div className="infoText">
+                    sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </div>
+                <div className="socialIcons" >
+                    <span className="icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="icon">
+                        <FaInstagram />
+                    </span>
+                    <span className="icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="icon">
+                        <FaLinkedin />
+                    </span>
+                </div>
+            </ContentWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
