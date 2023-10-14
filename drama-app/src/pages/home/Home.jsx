37e389187d543589be04c400faa25b0c1a762a37
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-10-14 18:56:09 +0300

    uu

--- drama-app/src/pages/home/Home.jsx
+++ drama-app/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.css'
 import HeroBanner from './hearobanner/HeroBanner'
+import Trending from './tranding/Trending'
 const Home = () => {
   return (
     <div>
 
       <HeroBanner/>
+      <Trending/>
     </div>
   )
 }
